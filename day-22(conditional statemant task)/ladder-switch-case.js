39b66a7117769316ab5df5075f924c58ce36305a
ghoniy panth <ghoniyapanth5@gmail.com>
2025-06-27 15:18:49 +0530

    Add files via upload

---
+++ day-22(conditional statemant task)/ladder-switch-case.js
@@ -0,0 +1,13 @@
+let fruit = "apple";
+switch (fruit) {
+  case "apple":
+  case "mango":
+    console.log("Sweet fruit");
+    break;
+  case "lemon":
+  case "orange":
+    console.log("Sour fruit");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
